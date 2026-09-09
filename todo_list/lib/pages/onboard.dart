4df6f8524913ef9b5e7ef350034b675ecf8fe7b4
Chrystie Rae Sajorne <casajorne@up.edu.ph>
2026-09-09 19:53:56 +0800

    feat: add theme colors

--- todo_list/lib/pages/onboard.dart
+++ todo_list/lib/pages/onboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/theme/colors.dart';
 
 class OnboardPage extends StatelessWidget {
   const OnboardPage({super.key});
@@ -34,7 +35,7 @@ class OnboardPage extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.w500,
-                  color: Color(0xFF555555),
+                  color: AppColors.accentColor,
                   height: 1.4,
                 ),
               ),
@@ -46,10 +47,9 @@ class OnboardPage extends StatelessWidget {
                 height: 60,
                 child: ElevatedButton(
                   onPressed: () {
-                    // tasks main page
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF5B16E8),
+                    backgroundColor: AppColors.mainButton,
                     foregroundColor: Colors.white,
                     elevation: 0,
                     shape: RoundedRectangleBorder(
---
+++ todo_list/lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+  
+  static const Color mainButton = Color(0xFF5B16E8);
+  static const Color background = Color(0xF4F5F7FF);
+  static const Color accentColor = Color(0xFF555555);
+}
